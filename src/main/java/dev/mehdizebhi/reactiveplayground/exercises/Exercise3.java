6e64e76047d2da9dbfb3ff92b4f95afe1f9f32c5
mehdizebhi <mehdizebhi@yahoo.com>
2024-02-22 10:23:42 +0330

    add Exercise3

---
+++ src/main/java/dev/mehdizebhi/reactiveplayground/exercises/Exercise3.java
@@ -0,0 +1,25 @@
+package dev.mehdizebhi.reactiveplayground.exercises;
+
+import dev.mehdizebhi.reactiveplayground.sources.ReactiveSources;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Exercise3 {
+
+    public static void main(String[] args) throws IOException {
+
+        // Use ReactiveSources.intNumbersFlux()
+
+        // Get all numbers in the ReactiveSources.intNumbersFlux stream
+        // into a List and print the list and its size
+        List<Integer> list = ReactiveSources.intNumbersFlux().toStream().toList();
+
+        System.out.println(list);
+        System.out.println(list.size());
+
+
+//        System.out.println("Press a key to end");
+//        System.in.read();
+    }
+}
