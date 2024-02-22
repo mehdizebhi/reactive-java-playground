b27445819916ee9d869d89991c6787849169deb1
mehdizebhi <mehdizebhi@yahoo.com>
2024-02-22 10:25:34 +0330

    add Exercise9

---
+++ src/main/java/dev/mehdizebhi/reactiveplayground/exercises/Exercise9.java
@@ -0,0 +1,27 @@
+package dev.mehdizebhi.reactiveplayground.exercises;
+
+import dev.mehdizebhi.reactiveplayground.sources.ReactiveSources;
+
+import java.io.IOException;
+
+public class Exercise9 {
+
+    public static void main(String[] args) throws IOException {
+
+        // Use ReactiveSources.intNumbersFlux()
+
+        // Print size of intNumbersFlux after the last item returns
+        //ReactiveSources.intNumbersFlux().count().subscribe(System.out::println);
+
+        // Collect all items of intNumbersFlux into a single list and print it
+        //ReactiveSources.intNumbersFlux().collectList().subscribe(System.out::println);
+
+        // Transform to a sequence of sums of adjacent two numbers
+        ReactiveSources.intNumbersFlux().buffer(2)
+                .map(l -> l.get(0) + l.get(1))
+                .subscribe(System.out::println);
+
+        System.out.println("Press a key to end");
+        System.in.read();
+    }
+}
