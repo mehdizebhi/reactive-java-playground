5c59f5eaf544d3066e83439456752c84a8447561
mehdizebhi <mehdizebhi@yahoo.com>
2024-02-22 10:23:28 +0330

    add Exercise2

---
+++ src/main/java/dev/mehdizebhi/reactiveplayground/exercises/Exercise2.java
@@ -0,0 +1,22 @@
+package dev.mehdizebhi.reactiveplayground.exercises;
+
+import dev.mehdizebhi.reactiveplayground.sources.ReactiveSources;
+
+import java.io.IOException;
+
+public class Exercise2 {
+
+    public static void main(String[] args) throws IOException {
+
+        // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
+
+        // Print all numbers in the ReactiveSources.intNumbersFlux stream
+        ReactiveSources.intNumbersFlux().subscribe(num -> System.out.println(num));
+
+        // Print all users in the ReactiveSources.userFlux stream
+        ReactiveSources.userFlux().subscribe(user -> System.out.println(user));
+
+        System.out.println("Press a key to end");
+        System.in.read();
+    }
+}
